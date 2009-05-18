d0c332a02e6f2ef6f5ceac9a6475b868961cadbf
Suraj Reddy <sixstring@gmail.com>
2009-05-18 19:26:08 -0400

    Added ajax features

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,10 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(params[:comment])
+    respond_to do |format|
+      format.html { redirect_to @post }
+      format.js
+    end
     # debugger
-    redirect_to @post
   end
 end
